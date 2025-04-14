5164769faf98b8505ee5ae145b1607a13dcc9c48
heykert.A <heykert.barrios@escola.pr.gov.br>
2025-04-14 09:09:18 -0300

    inicio javacript

---
+++ img/main.js
@@ -0,0 +1,13 @@
+let nome_usuario="";
+let element= document.querySelector(nome_usuario);
+while (nome_usuario = =" "){
+    nome_usuario = prompt (Qual e o seu nome?);
+
+}
+if (nome_usuario = = null){
+    element.textContent = "Seja muito bem vindo!"
+
+}
+else{
+    element.textContent = "nome-usuario"
+}
